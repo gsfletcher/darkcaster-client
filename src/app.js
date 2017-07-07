04fd7544ab46c4dd992cfe9295433bab0dbd3e93
Gizelle Fletcher <zelfletch@gmail.com>
2017-07-07 19:34:59 -0400

    created the current weather component with layout and controller. Added weather data for mocking purposes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
+/*jshint esversion:6*/
 const angular = require('angular');
+const weatherData = require('../mocks/weather.json');
 
 //create our application
 angular.module('darkcaster-client', []); //setter syntax
 
-angular.module('darkcaster-client') //getter syntax-angular's versin of 'require'
+angular.module('darkcaster-client') //getter syntax-angular's version of 'require'
        .controller('MainController', MainController);
 
 MainController.$inject = []; //magic for us
 
 function MainController(){
   this.message = 'hello from angular';
-
+  this.weatherData = weatherData.currently;
 }
