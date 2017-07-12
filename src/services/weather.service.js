79404502b2df5fa80d076188051efff6d09e7a2f
Gizelle Fletcher <zelfletch@gmail.com>
2017-07-11 20:09:12 -0400

    created the weather service that is currently mocked using our mock weather data. Changed the main controller to use this service to obtain said data

---
+++ src/services/weather.service.js
@@ -0,0 +1,15 @@
+/*jshint esversion:6*/
+const weatherData = require('../../mocks/weather.json');
+WeatherService.$inject = [];
+
+function WeatherService(){
+  return {
+  // label              function name
+    getCurrentWeather: getCurrently
+  };
+  function getCurrently(){
+    return weatherData.currently;
+  }
+}
+
+module.exports = WeatherService;
