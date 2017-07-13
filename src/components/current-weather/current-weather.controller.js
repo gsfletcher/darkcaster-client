14cc60094c24edde4504e183e21f4713615df69b
Gizelle Fletcher <zelfletch@gmail.com>
2017-07-12 20:34:42 -0400

    added http requests to the weather service and changed the controller to match change in weather service method

--- src/components/current-weather/current-weather.controller.js
+++ src/components/current-weather/current-weather.controller.js
@@ -1,13 +1,13 @@
+/*jshint esversion:6*/
 const clearDay = require('../../images/clear-day.png');
 MainController.$inject = ['WeatherService']; //magic for us
 
 function MainController(weather){
   this.message = 'hello from angular';
-  console.log(clearDay);
   this.imageLookup = {
     'clear-day': clearDay
   };
-  this.weatherData = weather.getCurrentWeather();
-
-}
+  weather.getCurrentWeather(29, -81) // this is a promise to get the weather ata back some time in the future, or might not
+         .then(currentWeather => this.weatherData =currentWeather);
+}           //assigns currentWeather to weatherData
 module.exports= MainController;
--- src/services/weather.service.js
+++ src/services/weather.service.js
@@ -1,14 +1,19 @@
 /*jshint esversion:6*/
 const weatherData = require('../../mocks/weather.json');
-WeatherService.$inject = [];
+WeatherService.$inject = ['$http'];
 
-function WeatherService(){
+function WeatherService($http){
+  const baseUrl = 'https://dry-eyrie-43885.herokuapp.com/weather/';
   return {
   // label              function name
     getCurrentWeather: getCurrently
   };
-  function getCurrently(){
-    return weatherData.currently;
+  function getCurrently(lat, lon){
+    const url = `${baseUrl}${lat},${lon}`;
+    return $http.get(url)
+                .then(response => {
+                  return response.data.currently;
+                });
   }
 }
 
